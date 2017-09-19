85da6cbe0e163f02e0a1a249728623a27247db60
angelRacancoj <the1999evilrg@gmail.com>
2017-09-19 05:36:14 -0600

    Add files via upload

---
+++ DetectorLexicoLF1/src/detectorlexicolf1/run/DetectorLexicoLF1.java
@@ -0,0 +1,16 @@
+package detectorlexicolf1.run;
+
+/**
+ *
+ * @author angelrg
+ */
+public class DetectorLexicoLF1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
